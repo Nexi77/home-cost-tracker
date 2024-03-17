a88ddfeba6de3ee5f67198ce364aeefe1abcbbff
Konrad Kolasa <kolasakonrad1@gmail.com>
2024-03-17 13:15:06 +0100

    feat(auth): add login page

---
+++ pages/login.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+definePageMeta({
+    layout: 'logged-out'
+});
+</script>
+
+<template>
+    <main class="login-site">
+       <section class="intro">
+            <h1>Welcome!</h1>
+            <p>You can use the form here, to log in and take control of your finances easily, don't waste more time, as time is money.</p>
+       </section>
+       <section class="form">
+            <UiAction variant="accent" kind="link" href="register">Wanna join? Sign up</UiAction>
+            <UiCard>
+                <form class="column-flow">
+                    <input type="email" placeholder="Email Address" />
+                    <input type="password" placeholder="Password" />
+                    <UiAction type="submit">
+                        <span>Sign in</span>
+                    </UiAction>
+                </form>
+            </UiCard>
+       </section>
+    </main>
+</template>
